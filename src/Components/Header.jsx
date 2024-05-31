049a6a743af9c3a279eecbe6ce9c7e6fbd7d65dc
satan-furation <satan.sharma@furation.tech>
2024-05-31 20:41:58 +0530

    image in hader problem resolve

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -10,6 +10,7 @@ import {
 } from "@chakra-ui/react";
 import { useEffect, useRef } from "react";
 import { init } from "ityped";
+import CoderBoy from "../Utilities/coder_boy.gif";
 
 export function Header() {
   const textRef = useRef();
@@ -97,11 +98,7 @@ export function Header() {
         </Box>
       </GridItem>
       <GridItem>
-        <Image
-          width={"100%"}
-          borderRadius="5px"
-          src="https://camo.githubusercontent.com/c1dcb74cc1c1835b1d716f5051499a2814c683c806b15f04b0eba492863703e9/68747470733a2f2f63646e2e6472696262626c652e636f6d2f75736572732f3733303730332f73637265656e73686f74732f363538313234332f6176656e746f2e676966"
-        />
+        <Image width={"100%"} borderRadius="5px" src={CoderBoy} />
       </GridItem>
     </Grid>
   );
